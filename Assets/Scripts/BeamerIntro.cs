539996b40c45548f43c53bb7e8474ec29f5c443d
Tom Martens <tommar49@gmail.com>
2019-10-10 10:44:11 +0200

    Scripts setup for intro

---
+++ Assets/Scripts/BeamerIntro.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using UnityEngine.Video;
+
+public class BeamerIntro : MonoBehaviour
+{
+    [SerializeField]
+    private VideoPlayer VideoSource;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Scripts/CountdownTimer.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using UnityEngine;
+
+public class CountdownTimer : MonoBehaviour
+{
+    public DateTime time;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
