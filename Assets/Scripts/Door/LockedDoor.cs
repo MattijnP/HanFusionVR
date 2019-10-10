d602a457228682b3d09417730791c347ed5edde7
JurianB <j.baldenhofer@gmail.com>
2019-10-10 13:40:09 +0200

    Doors can now be unlocked by using a key

---
+++ Assets/Scripts/Door/LockedDoor.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LockedDoor : MonoBehaviour
+{
+    [SerializeField]
+    private string _keyTag = string.Empty;
+
+    [SerializeField]
+    private Rigidbody _door;
+
+    void Start()
+    {
+        _door.isKinematic = true;
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        Debug.Log(other.gameObject.tag);
+        if (other.gameObject.CompareTag(_keyTag))
+        {
+            _door.isKinematic = false;
+        }
+    }
+}
