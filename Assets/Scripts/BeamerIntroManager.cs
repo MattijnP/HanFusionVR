51cda2a6f9cadea6efd1525c5ec48d002b72bbd9
JurianB <j.baldenhofer@gmail.com>
2019-10-10 11:14:42 +0200

    Added spacial sound to video player, added a script which starts the player after a few seconds

---
+++ Assets/Scripts/BeamerIntroManager.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using UnityEngine.Video;
+using System.Collections;
+
+public class BeamerIntroManager : MonoBehaviour
+{
+    [SerializeField]
+    private float _playVideoAfterSeconds;
+    private VideoPlayer _videoSource;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _videoSource = GetComponent<VideoPlayer>();
+
+        StartCoroutine(PlayVideoDelay());
+    }
+
+    IEnumerator PlayVideoDelay()
+    {
+        yield return new WaitForSeconds(_playVideoAfterSeconds);
+
+        _videoSource.Play();
+    }
+}
